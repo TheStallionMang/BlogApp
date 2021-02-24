06327c7201a429399a05635f028988dc07c1ea9c
Christian <barrosochristian3@gmail.com>
2021-02-24 14:39:43 -0800

    added username to registration and username shows in navbar

--- BlogApp/Data/Migrations/20210224013449_AddedAppUser.Designer.cs
+++ BlogApp/Data/Migrations/20210224013449_AddedAppUser.Designer.cs
@@ -4,14 +4,16 @@ using BlogApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210224013449_AddedAppUser")]
+    partial class AddedAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
